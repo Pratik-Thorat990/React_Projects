a29ec41f6d5991baf40242b76a188045cb5bc03c
Pratik Umesh Thorat <puthorat990>
2025-04-14 15:40:58 +0530

    LoginAndSignup

---
+++ Login_and _SignUp/abc/src/Components/LoginSignup/LoginSignup.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./LoginSignup.css" 
+
+import user_icon from "../Assets/user-line.png"
+import email_icon from "../Assets/mail-line.png"
+import password_icon from "../Assets/lock-2-line.png"
+const LoginSignup = () => {
+  return (
+    <div className="container">
+      <div className="header">
+        <div className="text">
+            Sign Up 
+        </div>
+        <div className="underline">
+
+        </div>
+        <div className="inputs">
+            <div className="input"></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LoginSignup
